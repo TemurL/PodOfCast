c4c22e0f0a34d806e6366984a7c0a930536fd594
Temur Lakerbaya <lakerbaya00@mail.ru>
2023-11-16 21:32:15 +0300

    better mob slider

--- src/js/modules/commentsHeightSet.js
+++ src/js/modules/commentsHeightSet.js
@@ -1,9 +1,11 @@
 export const commentsHeightSet = () => {
     setTimeout(() => {
         let commentHeightArr = Array.from(document.getElementsByClassName('comments__card')).map(comment => comment.offsetHeight);
-        console.log(commentHeightArr);
         let maxHeight = `${Math.max.apply(null, commentHeightArr)}px`;
-        console.log(maxHeight);
+        Array.from(document.getElementsByClassName('comments__card')).forEach(card => {
+            if (window.innerWidth > 768) return
+            card.style.height = maxHeight
+        });
         document.getElementsByClassName('comments__cards')[0].style.height = maxHeight;
     },100)
 }
