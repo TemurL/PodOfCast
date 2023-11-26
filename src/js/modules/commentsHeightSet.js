68826021dfca02073b783e97c633df406909220a
Temur Lakerbaya <lakerbaya00@mail.ru>
2023-11-26 16:50:50 +0300

    add form

--- src/js/modules/commentsHeightSet.js
+++ src/js/modules/commentsHeightSet.js
@@ -1,21 +1,17 @@
 export const commentsHeightSet = () => {
     setTimeout(() => {
+        let elemClass;
         let commentHeightArr;
-        if (document.title === 'Main') {
-            commentHeightArr = Array.from(document.getElementsByClassName('comments__card')).map(comment => comment.offsetHeight);
-        }
-        if (document.title === 'About') {
-            commentHeightArr = Array.from(document.getElementsByClassName('our-sponsors__card')).map(comment => comment.offsetHeight);
-        }
+
+        document.title === 'Main' ? elemClass = 'comments__card' : elemClass = 'our-sponsors__card';
+        
+        commentHeightArr = Array.from(document.getElementsByClassName(elemClass)).map(comment => comment.offsetHeight);
         let maxHeight = `${Math.max.apply(null, commentHeightArr)}px`;
         Array.from(document.getElementsByClassName('comments__card')).forEach(card => {
             if (window.innerWidth > 768) return
             card.style.height = maxHeight
         });
-        if (document.title === 'Main') {
-            document.getElementsByClassName('comments__cards')[0].style.height = maxHeight;
-        } else {
-            document.getElementsByClassName('our-sponsors__cards')[0].style.height = maxHeight;
-        }
+
+        document.title === 'Main' ? document.getElementsByClassName('comments__cards')[0].style.height = maxHeight : document.getElementsByClassName('our-sponsors__cards')[0].style.height = maxHeight;
     },100)
 }
